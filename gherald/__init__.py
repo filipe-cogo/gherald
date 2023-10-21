b2396a78d86b4e1805d39a26a32f80360b0f84f5
Filipe Cogo <filipe.cogo@gmail.com>
2023-10-21 09:30:57 -0400

    :tada: Initial commit

---
+++ gherald/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""A python package to assess the risk of a change during code review."""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
